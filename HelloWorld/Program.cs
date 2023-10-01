1c4b83306be07d4908b756251aa3516fd591ab4d
Arthur Diesel Ogg <arthurdiesel@outlook.com>
2023-10-01 13:11:25 -0300

    First and Second Chapter Concluded!

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,53 @@
+﻿/// XML Documentation!
+
+// using System;
+
+/*
+Without namespace, it's called top level statements!
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) // Entry point (begin the program here) + string[] args is arguments inputted with the program start!
+        {
+            */
+Console.WriteLine("Hello World!");
+//Console.WriteLine("What's your name?");
+// string? name = Console.ReadLine();
+// if (String.IsNullOrEmpty(name))
+// {
+//     Console.WriteLine("Please, input your name next time!");
+//     return;
+// }
+// Console.WriteLine("Hello, {0}!", name);
+/*
+}
+}
+}
+*/
+
+int i = 10;
+long bigi = (long)i;
+float f = 2.0f;
+double doub = (double)f;
+decimal d = 10.0m;
+bool b = true;
+char c = 'c';
+string str = "str";
+
+var x = 10; // Implicit!
+
+dynamic dyn = 10;
+dyn = true;
+
+int[] ints = new int[5];
+string[] strs = { "one", "two", "three" };
+
+object obj = null;
+
+string nullString = null;
+nullString ??= "Yes!"; // nullString is null? then...
+
+Console.WriteLine(obj ?? "Null!");
+Console.WriteLine(nullString);
+
---
+++ SecondChapter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SecondChapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? isFalse = null;
+            switch (isFalse)
+            {
+                case true:
+                    Console.WriteLine("Yes!");
+                    break;
+                case false:
+                    Console.WriteLine("No!");
+                    break;
+                default:
+                    Console.WriteLine("Maybe?");
+                    break;
+            }
+
+            string[] names = { "Arthur", "Gabriel", "Victor", "João" };
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
